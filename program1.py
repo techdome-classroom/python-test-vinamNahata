e909ec19999be6ce187e656d1256bfe559879953
VinamNahata <96768211+vinamNahata@users.noreply.github.com>
2024-01-05 13:03:22 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,12 +1,17 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+        stack = []
+    matching_pairs = {')': '(', '}': '{', ']': '['}
+        for char in s:
+        if char in matching_pairs:  # Closing parenthesis
+            if not stack or stack.pop() != matching_pairs[char]:
+                return False
+        else:  # Opening parenthesis
+            stack.append(char)
+
+    return not stack  # True if stack is empty
         pass
     
-
 
 
   
