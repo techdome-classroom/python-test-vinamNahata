2f155f1999cdad0e7b81f16380415c4e92145514
VinamNahata <96768211+vinamNahata@users.noreply.github.com>
2024-01-05 13:24:04 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -4,4 +4,22 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        # Dictionary to map Roman numerals to their values
+        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        
+        result = 0
+
+        for i in range(len(s)):
+            # If the current numeral is smaller than the next one, subtract its value
+            if i < len(s) - 1 and roman_dict[s[i]] < roman_dict[s[i + 1]]:
+                result -= roman_dict[s[i]]
+            else:
+                result += roman_dict[s[i]]
+
+        return result
+
+# Test cases
+solution = Solution()
+print(solution.romanToInt("III"))      # Output: 3
+print(solution.romanToInt("LVIII"))    # Output: 58
+print(solution.romanToInt("MCMXCIV"))  # Output: 1994
