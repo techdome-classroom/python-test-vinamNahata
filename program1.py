e770bffb1eec3150b30eda176bbcb4dc900b8d17
VinamNahata <96768211+vinamNahata@users.noreply.github.com>
2024-01-05 13:33:46 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,17 +1,19 @@
 class Solution(object):
     def isValid(self, s):
         stack = []
-    matching_pairs = {')': '(', '}': '{', ']': '['}
-        for char in s:
-        if char in matching_pairs:  # Closing parenthesis
-            if not stack or stack.pop() != matching_pairs[char]:
-                return False
-        else:  # Opening parenthesis
-            stack.append(char)
+        bracket_dict = {')': '(', '}': '{', ']': '['}
 
-    return not stack  # True if stack is empty
-        pass
-    
+        for char in s:
+            if char in bracket_dict.values():
+                stack.append(char)
+            elif char in bracket_dict.keys():
+                if not stack or stack.pop() != bracket_dict[char]:
+                    return False
+            else:
+                return False
+
+        return not stack
+
 
 
   
